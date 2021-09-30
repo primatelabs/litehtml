71b96b7609d75f8b8bccf57e846d6b27c1590a53
John Poole <john@primatelabs.com>
2021-09-30 09:39:59 -0400

    Add simple css_stylesheet unit test
    The unit test is meant to exercise css_stylesheet::parse_stylesheet().
    It does not test the output of parse_stylesheet() is correct, merely
    that it executes.

---
+++ src/litehtml/css/css_stylesheet_test.cpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2020-2021 Primate Labs Inc. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//    * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//    * Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "litehtml/css/css_stylesheet.h"
+
+#include <gtest/gtest.h>
+
+#include "litehtml/document.h"
+#include "litehtml/os_types.h"
+#include "test_container.h"
+
+using namespace litehtml;
+
+TEST(CSSStylesheetTest, Parse)
+{
+    tstring css =
+        _t("/* A simple CSS stylesheet */\n"
+           "body {\n"
+           "  margin: 25px;\n"
+           "  background-color: rgb(240,240,240);\n"
+           "  font-family: roboto, arial, sans-serif;\n"
+           "  font-size: 14px;\n"
+           "}\n");
+
+    test_container container;
+    litehtml::document::ptr doc =
+        std::make_shared<litehtml::document>(&container, nullptr);
+    css_stylesheet s;
+    media_query_list::ptr media;
+
+    s.parse_stylesheet(css.c_str(), "", doc, media);
+
+}
